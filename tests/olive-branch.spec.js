ce21247e1b6f68060259a4be7c87c72b4bd66b5d
xeffie <josefin.vingeskog@hotmail.com>
2026-09-23 12:30:17 +0200

    ci: automate end-to-end tests

--- tests/olive-branch.spec.js
+++ tests/olive-branch.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('should display Olive Branch heading', async ({ page }) => {
-    await page.goto('https://olive-branch-web-dev.onrender.com');
+    await page.goto('http://127.0.0.1:5500');
 
     await expect(
         page.getByRole('heading', { name: 'Olive Branch' })
@@ -9,22 +9,49 @@ test('should display Olive Branch heading', async ({ page }) => {
 });
 
 test('should filter organizations by category', async ({ page }) => {
-    await page.goto('https://olive-branch-web-dev.onrender.com');
+    await page.goto('http://127.0.0.1:5500');
+
+    await expect(
+        page.locator('#organizations article')
+    ).toHaveCount(5);
 
     await page.getByRole('button', { name: 'Children' }).click();
 
-    const organization = page.locator('#organizations article');
+    const organizations = page.locator('#organizations article');
 
-    await expect(organization).toBeVisible();
+    await expect(organizations).toHaveCount(1);
+
+    const organization = organizations.first();
+
     await expect(organization).toContainText('CHILDREN');
 
-    await expect(organization.getByRole('heading', { name: "Palestine Children's Relief Fund" })).toBeVisible();
+    await expect(
+        organization.getByRole('heading', {
+            name: "Palestine Children's Relief Fund"
+        })
+    ).toBeVisible();
 });
 
-test('should show message wgen category has no organizations', async ({ page }) => {
-    await page.goto('https://olive-branch-web-dev.onrender.com');
+test('should display medical organizations', async ({ page }) => {
+    await page.goto('http://127.0.0.1:5500');
+
+    await expect(
+        page.locator('#organizations article')
+    ).toHaveCount(5);
 
     await page.getByRole('button', { name: 'Medical' }).click();
 
-    await expect(page.getByText('No organizations available.')).toBeVisible();
+    const organizations = page.locator('#organizations article');
+
+    await expect(organizations).toHaveCount(1);
+
+    const organization = organizations.first();
+
+    await expect(organization).toContainText('MEDICAL');
+
+    await expect(
+        organization.getByRole('heading', {
+            name: 'Olive Medical Relief'
+        })
+    ).toBeVisible();
 });
